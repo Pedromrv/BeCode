2598553e92692887f69def48a1b12c7e52a0a3dd
Pedromrv <pedromrv@gmail.com>
2017-10-24 16:15:17 +0200

    Changes update

---
+++ Pierre-proyect/FoodForYou.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="A site of healthy raw food">
+	<meta name="keywords" content="raw, food, healthy, recipes">
+	<title>FoodForYou</title>
+<link rel="stylesheet" type="text/css" href="public/assets/css/pierrecss.css?1422585377">
+</head>
+
+<body>
+  <header id="main-header">
+
+  		<a id="logo-header" href="#">
+  			<span class="site-name">FoodForYou</span>
+  		</a> <!-- / #logo-header -->
+			<nav>
+  			<ul>
+  				<li><a href="#">Health</a></li>
+  				<li><a href="#">Raw Food</a></li>
+  				<li><a href="#">Recipes</a></li>
+					<li><a href="#">Shop</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">LOLOLO</a></li>
+  			</ul>
+  		</nav>
+		<!-- / nav -->
+
+  	</header>
+		<!-- / #main-header -->
+		<div class="menu">
+			<ul>
+  			<li>
+  			  <a href="#">Miau</a>
+			  </li>
+				<li>
+    			<a href="#">Item 2</a>
+  			</li>
+  			<li>
+    			<a href="#">Item 3</a>
+  			</li>
+  			<li>
+    			<a href="#">Item 4</a>
+  			</li>
+				<li>
+    			<a href="#">Item 5</a>
+  			</li>
+				<li>
+    			<a href="#">Item 6</a>
+			  </li>
+				<li>
+  			  <a href="#">Item 7</a>
+  			</li>
+				<li>
+  			  <a href="">Item 8</a>
+  			</li>
+				<li>
+    			<a href="">Item 9</a>
+  			</li>
+			</ul>
+		</div>
+
+<!--.menu -->
+<div id="page">
+	    <div id="main_content">
+  		<img src="images/fb-raw-food-diet.jpg" alt="">
+<h2>The Raw Food Diet: A Beginner's Guide and Review</h2>
+  <p>
+
+The raw food diet has been around since the 1800s, but has surged in popularity in recent years.
+Its supporters believe that consuming mostly raw foods is ideal for human health and has many benefits,
+ including weight loss and better overall health.<br>
+However, health experts warn that eating a mostly raw diet may lead to negative health consequences.
+This article reviews the good and bad of the raw food diet, as well as how it works.
+</p>
+<div id=imgcntr><img src="images/i-healed-6-year-illness-changing-diet.jpg" alt=""></div>
+<h2>What Is the Raw Food Diet?</h2>
+<p>
+The raw food diet, often called raw foodism or raw veganism, is composed of mostly or
+completely raw and unprocessed foods.
+A food is considered raw if it has never been heated over 104–118°F (40–48°C).
+It should also not be refined, pasteurized, treated with pesticides or otherwise processed in any way.
+Instead, the diet allows several alternative preparation methods, such as juicing, blending, dehydrating,
+ soaking and sprouting. Similar to veganism, the raw food diet is usually plant-based,
+being made up mostly of fruits, vegetables, nuts and seeds.<br>
+While most raw food diets are completely plant-based, some people also consume raw eggs and dairy.
+Less commonly, raw fish and meat may be included as well.
+Additionally, taking supplements is typically discouraged on the raw food diet.
+ Proponents often claim that the diet will give you all the nutrients you need.
+Supporters also believe that cooking foods is harmful to human health because it destroys the natural enzymes in foods,
+reduces their nutrient content and reduces the "life force" that they believe to exist in all raw or "living" foods.
+People follow the raw food diet for the benefits they believe it has, including weight loss, improved vitality,
+increased energy, improvement to chronic diseases, improved overall health and a reduced impact on the environment.
+</p>
+<div id=imgdwn><img src="images/images.jpeg" alt=""></div>
+<h2>How to Follow the Raw Food Diet</h2>
+<p>To follow the raw food diet, make sure at least 75% of the food you eat is raw.<br>
+Most raw food diets are made primarily of fruits, vegetables, nuts and seeds. Grains and legumes are often permitted as well,
+but usually need to be soaked or sprouted before you eat them.</p>
+</div>
+
+<!--#main_content  -->
+		<div id="foods">
+	  	<div class="FTE">
+	  		<ul>
+	    		<h2>Foods to Eat</h2>
+	    		<li>All fresh fruits</li>
+	    		<li>All raw vegetables</li>
+	    		<li>Raw nuts and seeds</li>
+	  		  <li>Raw grains and legumes, sprouted or soaked</li>
+	  		  <li>Dried fruits and meats</li>
+	  		  <li>Nut milks</li>
+	  		  <li>Raw nuts butters</li>
+	  		  <li>Cold-pressed olivw and coconut oils</li>
+			    <li>Fermented foods like kimchi and sauerkraut</li>
+	  		  <li>Seaweed</li>
+	  		  <li>Sprouts</li>
+	  			<li>Raw eggs or dairy, if desired</li>
+	  		  <li>Raw meat or fish, if desired</li>
+	  		</ul>
+	</div>
+		<div class="FTA">
+			  <ul>
+	    		<h2>Foods to Avoid</h2>
+	    		<li>Cooked fruits,vegetables, meats and grains</li>
+	  		  <li>Baked items</li>
+	  		  <li>Roasted nuts and seeds</li>
+	  		  <li>Refined oils</li>
+	  		  <li>Table salt</li>
+	    		<li>Refined sugars and flour</li>
+	    		<li>Pasteruized juices and dairy</li>
+	  		  <li>Coffe and tea</li>
+	  		  <li>Alcohol</li>
+	  		  <li>Pasta</li>
+	  		  <li>Pastries</li>
+	  		  <li>Chips</li>
+	  		  <li>Other processed foods and snacks</li>
+	  		</ul>
+		</div>
+	</div>
+</div>
+
+<!--#foods -->
+
+
+<!-- / #main-footer -->
+</body>
+</html>
---
+++ Pierre-proyect/RawFood.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="A site of healthy raw food">
+	<meta name="keywords" content="raw, food, healthy, recipes">
+	<title>RawFood</title>
+<link rel="stylesheet" type="text/css" href="public/assets/css/pierrecss.css?1422585377">
+</head>
+
+<body>
+  <header id="main-header">
+
+  		<a id="logo-header" href="#">
+  			<span class="site-name">FoodForYou</span>
+  		</a> <!-- / #logo-header -->
+			<nav>
+  			<ul>
+  				<li><a href="#">Health</a></li>
+  				<li><a href="#">Raw Food</a></li>
+  				<li><a href="#">Recipes</a></li>
+					<li><a href="#">Shop</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">LOLOLO</a></li>
+  			</ul>
+  		</nav>
+		<!-- / nav -->
+
+  	</header>
+		<!-- / #main-header -->
+		<div class="menu">
+			<ul>
+  			<li>
+  			  <a href="#">Whatever</a>
+			  </li>
+				<li>
+    			<a href="#">Item 2</a>
+  			</li>
+  			<li>
+    			<a href="#">Item 3</a>
+  			</li>
+  			<li>
+    			<a href="#">Item 4</a>
+  			</li>
+				<li>
+    			<a href="#">Item 5</a>
+  			</li>
+				<li>
+    			<a href="#">Item 6</a>
+			  </li>
+				<li>
+  			  <a href="#">Item 7</a>
+  			</li>
+				<li>
+  			  <a href="">Item 8</a>
+  			</li>
+				<li>
+    			<a href="">Item 9</a>
+  			</li>
+			</ul>
+		</div>
+
+<!--.menu -->
+
+<div id=rawfood>
+	<div>Every Designer’s Workplace Fantasy.</div>
+	<div>Promotional Advertising Specialty You have Waited Long Enough.</div>
+	<div>Get Best Advertiser In Your Side Pocket.</div>
+
+	<div>kjbdskfj</div>
+	<div>usdfhilds</div>
+	<div>iousdhfiouds</div>
+
+	<div>idushfiudsa</div>
+	<div>iuhsdfdas</div>
+	<div>sdfdsaafd</div>
+</div> <!--#rawfood-->
+</body>
+</html>
